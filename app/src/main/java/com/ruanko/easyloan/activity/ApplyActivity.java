2fffd37ccd295228d20457fd9ad1afa39b93ebd2
panjunwen <i@panjunwen.com>
2017-07-27 20:50:13 +0800

    modify login

---
+++ app/src/main/java/com/ruanko/easyloan/activity/ApplyActivity.java
@@ -0,0 +1,15 @@
+package com.ruanko.easyloan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ruanko.easyloan.R;
+
+public class ApplyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply);
+    }
+}
