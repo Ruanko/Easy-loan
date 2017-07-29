1f7026fa2563b6b0b5e2e67740f5d4037b715f4e
panjunwen <i@panjunwen.com>
2017-07-29 11:06:37 +0800

    add application

---
+++ app/src/main/java/com/ruanko/easyloan/activity/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.ruanko.easyloan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ruanko.easyloan.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
